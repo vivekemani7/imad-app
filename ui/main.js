5810c2049693ea4005d61c0eb1bc2592b2020f31
vivekemani7 <30910906+vivekemani7@users.noreply.github.com>
2017-08-26 23:42:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'THIS IS VIVEK';
 // move the element
-var img =  document.getElementById('img');
+var img =  document.getElementById('madi');
 img.oneclick = function(){
   img.style.marginleft = '100px';
 };
