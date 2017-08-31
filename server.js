10cba439842e68183d4b6c25f09e708e31e0db3f
vivekemani7 <30910906+vivekemani7@users.noreply.github.com>
2017-08-31 16:00:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     user: 'vivekemani7',
     database: 'vivekemani7',
     host: 'db.imad.hasura-app.io',
-    port: '5432',
+    port: 5432,
     password: process.env.DB_PASSWORD
 };
 var app = express();
