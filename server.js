0b5cbc369eb3990cdf6c6cc504794141d2b38547
vivekemani7 <30910906+vivekemani7@users.noreply.github.com>
2017-08-31 16:11:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,11 +86,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool(config) ;
+var pool = new Pool('config') ;
 app.get('/test_db', function (req, res){
     //make a select request
     //return a response with the results
-    pool.query('SELECT * FROM "test" LIMIT 50',function(err,result)
+    pool.query('SELECT * FROM test',function(err,result)
     {
        if(err){
            res.status(500).send(err.toString());
